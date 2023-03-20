54d0d688dce2a2e9813a1568c117d017c41e6186
GabrielAvelino-SA <gabriel22santosa@gmail.com>
2023-03-20 08:10:52 -0300

    refactor with MVC architecture

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    return "<p>API-Flask 0.1</p>"
+from app.controllers import default
 
 if __name__ == '__main__':
     app.run(debug=True, port="5000")
